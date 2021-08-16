ac55e96b5dd710563e63bfe1698004e7c2d4eff8
sdk <sdk1926@naver.com>
2021-08-16 12:00:26 +0900

    Add: movies application

--- movies/apps.py
+++ movies/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class OwnersConfig(AppConfig):
+class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'owners'
+    name = 'movies'
